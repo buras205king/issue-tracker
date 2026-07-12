f6df57b309f859196f20931021726cbdb7b15504
biruk asefa <birukasefa205@gmail.com>
2026-07-12 11:15:04 -0900

    style the acctive link in the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,33 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import{usePathname} from 'next/navigation'
 import { FcLibrary } from "react-icons/fc";
+import { text } from 'stream/iter';
 
 const NavBar = () => {
+    const currentpath = usePathname();
+    const links=[
+        {
+            label: "Dashboard",
+            href: "/"
+        },
+        {
+            label: "Issues",
+            href: "/issues"
+        }
+    ]
   return (
    <nav className='space-x-8 m-0 flex  border-4  border-green-900  border-9 border-r-8 p-3 rounded-full'>   
      <Link className='h-30 p-3' href="/"><FcLibrary /></Link>
      <ul className='flex space-x-8'>
-        <li><Link className='text-black hover:text-red-500 pt-10' href="/">Dashboard</Link></li>
-        <li><Link className='text-black hover:text-red-500 pt-10' href="/issues">issues</Link></li>
+        {links.map((link) => (
+            <li key={link.href}>
+                <Link className={`${link.href===currentpath? 'text-orange-500' : 'hover:text-red-500'} pt-10 transition-colors`} href={link.href}>
+                    {link.label}
+                </Link>
+            </li>
+        ))}
      </ul>
     </nav>
   )
